815c873c75cef52d2b38c6c17d84d9a85efc26f3
Bastian Bacher <bastianbacher@web.de>
2022-07-07 13:20:15 +0200

    update notification has no return

--- src/main/java/edu/kit/tm/cm/smartcampus/problemmanagement/infrastructure/connector/RestTemplateBuildingConnector.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/problemmanagement/infrastructure/connector/RestTemplateBuildingConnector.java
@@ -58,14 +58,12 @@ public class RestTemplateBuildingConnector implements BuildingConnector{
   }
 
   @Override
-  public Notification updateNotification(Notification notification) {
+  public void updateNotification(Notification notification) {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
     HttpEntity<Notification> entity = new HttpEntity<>(notification, headers);
 
     restTemplate.exchange(baseUrl + updateNotificationUrl, HttpMethod.PUT, entity, Void.class, notification.getIdentificationNumber());
-
-    return notification;
   }
 
   @Override
